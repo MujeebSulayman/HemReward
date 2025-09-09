99fddd3b378c364d6905d900438743fb5d362e2f
MujeebSulayman <sulaymanmujeeb6@gmail.com>
2025-09-09 17:20:34 +0100

    Update Hardhat configuration to use Sepolia network, removing Mumbai settings and adjusting Etherscan API key reference.

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,16 +2,12 @@ require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config();
 
 module.exports = {
-	defaultNetwork: 'mumbai',
+	defaultNetwork: 'sepolia',
 	networks: {
-		mumbai: {
-			url: process.env.MUMBAI_RPC_URL || 'https://polygon-mumbai.g.alchemy.com/v2/demo',
-			accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-			chainId: 80001,
-		},
 		sepolia: {
-			url: process.env.NEXT_PUBLIC_RPC_URL,
-			accounts: [process.env.PRIVATE_KEY],
+			url: process.env.SEPOLIA_RPC_URL || 'https://sepolia.infura.io/v3/your-project-id',
+			accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+			chainId: 11155111,
 		},
 		hardhat: {
 			chainId: 1337,
@@ -31,7 +27,7 @@ module.exports = {
 	},
 	etherscan: {
 		apiKey: {
-			polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+			sepolia: process.env.ETHERSCAN_API_KEY,
 		},
 	},
 };
